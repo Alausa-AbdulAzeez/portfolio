3842f947cbd6e11028354b37e69997da850f8cd2
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-07-28 17:37:53 +0100

    changed download btn to view btn

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+// import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download='cv' className='btn'>
-        Download CV
+      <a
+        href='https://drive.google.com/file/d/18gps8HeprMCYfkiibee_CiOZXmjsPF14/view?usp=sharing'
+        // download='cv'
+        target='_blank'
+        className='btn'
+        rel='noreferrer'
+      >
+        View Resume
       </a>
       <a href='#contact' className='btn btn-primary'>
         Let's Talk
