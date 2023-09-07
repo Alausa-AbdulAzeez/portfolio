6f8170e0d1f7a843fba48cd928847aaf5a76c5b8
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-07 17:30:20 +0100

    added the 3ui projects

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,87 +1,108 @@
-import React from "react";
-import "./experience.css";
+import React from 'react'
+import './experience.css'
 
-import { BsPatchCheckFill } from "react-icons/bs";
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">
+    <section id='experience'>
       <h5>Skills</h5>
       <h2>My Experience</h2>
+      <div className='workExperience container'>
+        <h3>
+          Lifeworth Hmo <span className='durationSpan'>2022 - Present</span>
+        </h3>
+        <p className='titleText'>Frontend Developer</p>
+        <ul>
+          <li className='workExperienceList'>
+            Spearheaded the development of the front-end of a cutting-edge
+            Laboratory Information Management System (LIMS).
+          </li>
+          <li className='workExperienceList'>
+            Notably resolved critical bugs and completed a flawless front-end
+            experience of a claims vetting portal.
+          </li>
+          <li className='workExperienceList'>
+            Demonstrated technical prowess by seamlessly integrating a
+            telemedicine service into our mobile app using React Native,
+            enhancing its functionality.
+          </li>
+        </ul>
+      </div>
 
-      <div className="container experience__container">
-        <div className="experience__frontend">
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
           <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>CSS</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Javascript</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Bootstrap</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Tailwind</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>SCSS</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>React</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>NextJS</h4>
-                <small className="text-light">Experienced</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Typescript</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Figma</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
           </div>
@@ -140,49 +161,49 @@ const Experience = () => {
             </article>
           </div>
         </div> */}
-        <div className="experience__backend">
+        <div className='experience__backend'>
           <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>NodeJS</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Express JS</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>MongoDB</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>PostgresSQL</h4>
-                <small className="text-light">Beginner</small>
+                <small className='text-light'>Beginner</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div className="">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div className=''>
                 <h4>Python</h4>
-                <small className="text-light">Beginner</small>
+                <small className='text-light'>Beginner</small>
               </div>
             </article>
           </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Experience;
+export default Experience
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,18 +1,52 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/AdminDashboard.png'
+import IMG1 from '../../assets/3dSite.png'
 import IMG2 from '../../assets/HMS.png'
-import IMG3 from '../../assets/Screenshot (6).png'
+import IMG3 from '../../assets/nike.png'
+import IMG6 from '../../assets/metaverse.png'
 import IMG4 from '../../assets/BiopathProject.png'
 import IMG5 from '../../assets/lwPortal.png'
 
 const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: '3D-Customization',
+    github: 'https://github.com/Alausa-AbdulAzeez/3DShirtCustomizatonClient',
+    demo: 'https://3-d-shirt-customizaton-client.vercel.app/',
+    tools: ['React', 'Tailwind', 'threeJS', 'Framer-Motion'],
+    details:
+      'Designed an amazing 3D shirt customization tool using Three.js for immersive 3D experiences, Framer Motion for stunning animations, and Tailwind CSS for sleek and responsive design.',
+  },
+  {
+    id: 2,
+    image: IMG3,
+    title: 'Nike Landing Page',
+    github: 'https://github.com/Alausa-AbdulAzeez/nikeLandingPage',
+    demo: 'https://nike-eight-kohl.vercel.app/',
+    tools: ['React', 'Tailwind'],
+    details:
+      'Crafted an impressive and fully responsive landing page using ReactJS and Tailwind CSS, serving as a captivating showcase of my exceptional CSS proficiency.',
+  },
+  {
+    id: 3,
+    image: IMG6,
+    title: 'Metaverse',
+    github: 'https://github.com/Alausa-AbdulAzeez/metaverse',
+    demo: 'https://metaverse-pi-lake.vercel.app/',
+    tools: ['NextJS', 'Framer-motion', 'Tailwind'],
+    details:
+      'Crafted a fully responsive and contemporary Next.js website  enriched with captivating animations brought to life by the dynamic capabilities of Framer Motion',
+  },
   {
     id: 4,
     image: IMG4,
     title: 'LIMS',
     github: '',
     demo: 'https://staff.biopathonline.com/',
+    tools: ['React', 'SCSS', 'Redux-Tool-Kit'],
+    details:
+      'It enabled seamless collaboration among various laboratory sections while also serving as a dynamic data management tool.',
   },
   {
     id: 5,
@@ -20,6 +54,9 @@ const data = [
     title: 'Claims Vetting Portal',
     github: '',
     demo: 'https://admin.lifeworthhmoportal.com/',
+    tools: ['React', 'SCSS', 'Redux-Tool-Kit'],
+    details:
+      'Claims Processing Portal, replacing a costly third-party application.',
   },
   {
     id: 2,
@@ -27,21 +64,23 @@ const data = [
     title: 'HMS',
     github: 'https://github.com/Alausa-AbdulAzeez/hms',
     demo: 'https://magical-choux-f4a46f.netlify.app/',
+    tools: ['React', 'SCSS', 'Typescript'],
+    details: 'A hospital management system currently being built',
   },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'PROMPTOPIA',
-    github: 'https://github.com/Alausa-AbdulAzeez/prompt',
-    demo: 'https://prompt-gamma.vercel.app/',
-  },
-  {
-    id: 1,
-    image: IMG1,
-    title: 'Admin Dashboard',
-    github: 'https://github.com/Alausa-AbdulAzeez/adminDashboard',
-    demo: 'https://graceful-sunshine-fa7e76.netlify.app/',
-  },
+  // {
+  //   id: 3,
+  //   image: IMG3,
+  //   title: 'PROMPTOPIA',
+  //   github: 'https://github.com/Alausa-AbdulAzeez/prompt',
+  //   demo: 'https://prompt-gamma.vercel.app/',
+  // },
+  // {
+  //   id: 1,
+  //   image: IMG1,
+  //   title: 'Admin Dashboard',
+  //   github: 'https://github.com/Alausa-AbdulAzeez/adminDashboard',
+  //   demo: 'https://graceful-sunshine-fa7e76.netlify.app/',
+  // },
 ]
 
 const Portfolio = () => {
@@ -61,26 +100,36 @@ const Portfolio = () => {
                   className='portfolio__item_image'
                 />
               </div>
-              <h3>{singleItem.title}</h3>
-              <div className='portfolio__item-cta'>
-                {singleItem.github !== '' && (
+              <div className='bottomCardWrapper'>
+                <h3 className='projectTitle'>{singleItem.title}</h3>
+                <div className='detailsAndToolsWrapper'>
+                  <p className='projectDetails'>{singleItem?.details}</p>
+                  <div className='toolsWrapper'>
+                    {singleItem?.tools?.map((tool) => {
+                      return <div className='tool'>{tool}</div>
+                    })}
+                  </div>
+                </div>
+                <div className='portfolio__item-cta'>
+                  {singleItem.github !== '' && (
+                    <a
+                      href={singleItem.github}
+                      className='btn'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Github
+                    </a>
+                  )}
                   <a
-                    href={singleItem.github}
-                    className='btn'
+                    href={singleItem.demo}
+                    className='btn btn-primary'
                     target='_blank'
                     rel='noreferrer'
                   >
-                    Github
+                    Live Demo
                   </a>
-                )}
-                <a
-                  href={singleItem.demo}
-                  className='btn btn-primary'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Live Demo
-                </a>
+                </div>
               </div>
             </article>
           )
