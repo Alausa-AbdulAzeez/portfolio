c381d579418c310af16de478d95805fa1ce7f267
Alausa-AbdulAzeez <87964160+Alausa-AbdulAzeez@users.noreply.github.com>
2024-05-30 02:03:26 -0700

    Updated portfolio link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,8 @@ const CTA = () => {
     <div className='cta'>
       <a
         // href='https://drive.google.com/file/d/18gps8HeprMCYfkiibee_CiOZXmjsPF14/view?usp=sharing'
-        href='https://docs.google.com/document/d/18P4gwCgvFQolz4Sr-8xqOb2dp-AQnzELBc9U3f-keps/edit?usp=sharing'
+        // href='https://docs.google.com/document/d/18P4gwCgvFQolz4Sr-8xqOb2dp-AQnzELBc9U3f-keps/edit?usp=sharing'
+        href='https://drive.google.com/file/d/1ZE4GfdoUvU1sdqtLzkSVAn94mOwkOBCV/view'
         // href='https://drive.google.com/file/d/1g3mwP2yUMJMeziBjptSVrAeaEkm5EcRp/view?usp=sharing'
         // download='cv'
         target='_blank'
