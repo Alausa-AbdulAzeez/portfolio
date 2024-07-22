9ce3180784065e342630a245e15488ccc06e494e
Alausa <alausaabdulazeez@gmail.com>
2024-07-22 09:46:06 +0100

    changed exp years

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,33 +1,33 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import './about.css'
-import { FaAward } from 'react-icons/fa'
-import { VscFolderLibrary } from 'react-icons/vsc'
+import "./about.css";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
-      <div className='container about__container'>
-        <div className='about__me'>
-          <div className='about__me-image'>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
             <img
-              src='https://freesvg.org/img/1547510251.png'
-              alt='About Image'
-              className='aboutImage'
+              src="https://freesvg.org/img/1547510251.png"
+              alt="About Image"
+              className="aboutImage"
             />
           </div>
         </div>
-        <div className='about__content'>
-          <div className='about__cards'>
-            <article className='about__card'>
-              <FaAward className='about__icon' />
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>2+ Years</small>
+              <small>3+ Years</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>30+</small>
             </article>
@@ -36,13 +36,13 @@ const About = () => {
             An avid and goal-driven full stack web developer with the savvy of
             building functional fullstack web applications.
           </p>
-          <a href='#contact' className='btn btn-primary'>
+          <a href="#contact" className="btn btn-primary">
             Get In Touch
           </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
